1fcaae6b61598afaabdc01e594071072194edf3c
Neha Jaiswal <nehaas1900@gmail.com>
2022-09-03 19:28:13 +0530

    logical and ternary operator

---
+++ Web22-logical-operators-and-ternary-operators/problem4.js
@@ -0,0 +1,13 @@
+// Problem 4: Given 3 numbers (all different values), print which is greatest
+let a=10;
+let b=15;
+let c=14;
+if(a>b && a>c){
+  console.log("a is greatest")
+}
+else if(b>a && b>c){
+  console.log("b is greatest")
+}
+else{
+  console.log("c is greatest")
+}
---
+++ Web22-logical-operators-and-ternary-operators/problem5.js
@@ -0,0 +1,20 @@
+// Problem 5: Given the days of the week in short format "Sun", "Mon" ... print in long format "Sunday", "Monday", ...
+
+let day="Mon"
+
+switch(day){
+  case "Sun":console.log("Sunday")
+  break
+  case "Mon":console.log("Monday")
+  break
+  case "Tue":console.log("Tuesday")
+  break
+  case "Wed":console.log("Wednesday")
+  break
+  case "Thur":console.log("Thursday")
+  break
+  case "Fri":console.log("Friday")
+  break
+  case "Sat":console.log("Saturday")
+  break
+}
